99c48eb5e160b98d6864e6f3c953f66533749635
hkimca <harrison.kim.ca@gmail.com>
2017-06-30 21:28:03 -0400

    Initial Commit

---
+++ 2w1d5_pigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  2w1d5_pigLatin
+//
+//  Created by Seantastic31 on 30/06/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
