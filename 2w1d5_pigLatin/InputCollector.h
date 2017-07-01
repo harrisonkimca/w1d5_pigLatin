0b8e31bd0b13e750b01c773021533f462af96c0e
hkimca <harrison.kim.ca@gmail.com>
2017-06-30 21:32:18 -0400

    Initial commit

---
+++ 2w1d5_pigLatin/InputCollector.h
@@ -0,0 +1,13 @@
+//
+//  InputCollector.h
+//  2w1d5_pigLatin
+//
+//  Created by Seantastic31 on 30/06/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InputCollector : NSObject
+
+@end
---
+++ 2w1d5_pigLatin/NSString+PigLatin.m
@@ -0,0 +1,13 @@
+//
+//  NSString+PigLatin.m
+//  2w1d5_pigLatin
+//
+//  Created by Seantastic31 on 30/06/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import "NSString+PigLatin.h"
+
+@implementation NSString (PigLatin)
+
+@end
